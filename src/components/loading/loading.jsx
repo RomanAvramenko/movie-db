7fb8066fd7c600175fd903bff34a61ce6c15ef03
unknown <ashkaell24@gmail.com>
2019-12-08 23:58:18 +0200

    added loader

---
+++ src/components/loading/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "./loading.scss";
+
+const Loading = () => {
+    return (
+        <div class="loadingio-spinner-pulse-ev73jk8wcgi"><div class="ldio-3hbtsrdoynl">
+            <div></div><div></div><div></div>
+        </div></div>
+    )
+}
