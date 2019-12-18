b7534d59ce368cf2d940c157e34344b0f453886b
unknown <ashkaell24@gmail.com>
2019-12-18 22:09:03 +0200

    added features

--- src/components/catalog/catalog.jsx
+++ src/components/catalog/catalog.jsx
@@ -7,17 +7,24 @@ import { TileItem } from '../tile-item/tile-item';
 
 export default class Catalog extends React.Component {
     state = {
-        response: []
+        response: [],
+        searchVars: '/popular' //
     }
 
     componentDidMount() {
         this.request();
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.searchVars !== prevState.searchVars) {
+            this.request();
+        }
+    }
+
     request = () => {
         const API_KEY = 'api_key=82d1a8c492becf617a26326954e61f9a';
         const BASE_URL = 'https://api.themoviedb.org/3/movie';
-        const url = `${BASE_URL}/popular?${API_KEY}&language=en-US&page=1`;
+        const url = `${BASE_URL}${this.state.searchVars}?${API_KEY}&language=en-US&page=1`;
         axios
             .get(url)
             .then(result => { this.setState({ response: this._transformData(result) }) })
@@ -31,6 +38,12 @@ export default class Catalog extends React.Component {
         }
     }
 
+    changeSearchHandler = (newSearch) => {
+        this.setState({
+            searchVars: newSearch
+        })
+    }
+
     render() {
         if (this.state.response.results === undefined) {
             return (
@@ -42,20 +55,17 @@ export default class Catalog extends React.Component {
                 <div className="catalog__header">
                     <ul className="catalog__menu menu">
                         <li className="menu__item">
-                            <button className="menu__btn menu__btn_active">Poular</button>
+                            <button className="menu__btn menu__btn_active" onClick={this.changeSearchHandler.bind(this, '/popular')}>Poular</button>
                         </li>
                         <li className="menu__item">
-                            <button className="menu__btn">Top Rated</button>
+                            <button className="menu__btn" onClick={this.changeSearchHandler.bind(this, '/top_rated')}>Top Rated</button>
                         </li>
                         <li className="menu__item">
-                            <button className="menu__btn">New Arrivals</button>
-                        </li>
-                        <li className="menu__item">
-                            <button className="menu__btn">Genre<i className="fas fa-angle-down"></i></button>
+                            <button className="menu__btn" onClick={this.changeSearchHandler.bind(this, '/upcoming')}>New Arrivals</button>
                         </li>
                     </ul>
                 </div>
-                <TileItem results={this.state.response.results}/>
+                <TileItem results={this.state.response.results} />
                 <Footer />
             </section>
         );
