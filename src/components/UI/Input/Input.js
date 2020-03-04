980c1e5bf1314e2190d25522077fc5a0ceee01ee
unknown <ashkaell24@gmail.com>
2020-03-04 21:49:38 +0200

    login form change styles

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Input.scss'
 
-export const Input = ({type, placeholder, name}) => {
-    return (
-        <input
-        className="input" type={type} placeholder={placeholder} name={name}
-        />
-    )
+export const Input = ({ type, placeholder, name }) => {
+  return (
+    <input
+      className="input"
+      type={type}
+      placeholder={placeholder}
+      name={name}
+    />
+  )
 }
