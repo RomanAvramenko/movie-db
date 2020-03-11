d8d7e6a72f4556992759de183295e88727996eca
unknown <ashkaell24@gmail.com>
2020-03-11 20:42:52 +0200

    add signUp signIn errors

--- src/components/LoginForm/submit.js
+++ src/components/LoginForm/submit.js
@@ -1,6 +1,9 @@
 import { SubmissionError } from 'redux-form'
 import axios from 'axios'
 
+const API_KEY = 'AIzaSyDbcN_rzgVmqLsfTCC-BxxqQudubKDGTSo'
+const URL_MAIN = 'https://identitytoolkit.googleapis.com/v1/accounts'
+
 export const signUp = async (values) => {
   const signUpData = {
     email: values.email,
@@ -8,10 +11,15 @@ export const signUp = async (values) => {
     returnSecureToken: true
   }
   try {
-    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDbcN_rzgVmqLsfTCC-BxxqQudubKDGTSo', signUpData)
+    const response = await axios
+      .post(`${URL_MAIN}:signUp?key=${API_KEY}`, signUpData)
     console.log(response);
-  } catch (error) {
-    console.log(error);
+  } catch (e) {
+    if (e.response.data.error.message === "EMAIL_EXISTS") {
+      throw new SubmissionError({
+        username: 'This email address already exists'
+      })
+    }
   }
 }
 
@@ -22,9 +30,24 @@ export const signIn = async (values) => {
     returnSecureToken: true
   }
   try {
-    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDbcN_rzgVmqLsfTCC-BxxqQudubKDGTSo', signInData)
+    const response = await axios
+      .post(`${URL_MAIN}:signInWithPassword?key=${API_KEY}`, signInData)
     console.log(response);
   } catch (e) {
-    console.log(e);
+    if (e.response.data.error.message === "INVALID_EMAIL") {
+      throw new SubmissionError({
+        username: 'User does not exist',
+        _error: 'Login failed!'
+      })
+    }
+    if (e.response.data.error.message === "INVALID_PASSWORD") {
+      throw new SubmissionError({
+        password: 'Wrong password',
+        _error: 'Login failed!'
+      })
+    }
+    console.log(e.response.data.error)
   }
 }
+
+
